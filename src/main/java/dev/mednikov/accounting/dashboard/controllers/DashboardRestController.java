ccb2d623cab4c04fb0f7dd5a1c024471d10875bb
Iurii Mednikov <yurimednikov@proton.me>
2025-06-18 11:24:55 +0200

    Backend: added DashboardRestController

---
+++ src/main/java/dev/mednikov/accounting/dashboard/controllers/DashboardRestController.java
@@ -0,0 +1,45 @@
+package dev.mednikov.accounting.dashboard.controllers;
+
+import dev.mednikov.accounting.reports.dto.ExpenseCategoryDto;
+import dev.mednikov.accounting.reports.dto.NetWorthSummaryDto;
+import dev.mednikov.accounting.reports.dto.ProfitLossSummaryDto;
+import dev.mednikov.accounting.reports.services.ExpenseCategoryService;
+import dev.mednikov.accounting.reports.services.NetWorthService;
+import dev.mednikov.accounting.reports.services.ProfitLossService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/dashboard")
+public class DashboardRestController {
+
+    private final ProfitLossService profitLossService;
+    private final NetWorthService netWorthService;
+    private final ExpenseCategoryService expenseCategoryService;
+
+    public DashboardRestController(
+            ProfitLossService profitLossService,
+            NetWorthService netWorthService,
+            ExpenseCategoryService expenseCategoryService) {
+        this.profitLossService = profitLossService;
+        this.netWorthService = netWorthService;
+        this.expenseCategoryService = expenseCategoryService;
+    }
+
+    @GetMapping("/profit-loss/{organizationId}")
+    public @ResponseBody ProfitLossSummaryDto getProfitLossSummary (@PathVariable Long organizationId) {
+        return this.profitLossService.getProfitLossSummary(organizationId);
+    }
+
+    @GetMapping("/net-worth/{organizationId}")
+    public @ResponseBody NetWorthSummaryDto getNetWorthSummary(@PathVariable Long organizationId) {
+        return this.netWorthService.getNetWorthSummary(organizationId);
+    }
+
+    @GetMapping("/expense-categories/{organizationId}")
+    public @ResponseBody List<ExpenseCategoryDto> getExpenseCategories (@PathVariable Long organizationId) {
+        return this.expenseCategoryService.getExpenseCategories(organizationId);
+    }
+
+}
