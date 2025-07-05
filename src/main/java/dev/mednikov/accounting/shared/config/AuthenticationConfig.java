02f671f2555185f427f798aecbbb9fc90abac310
Iurii Mednikov <yurimednikov@proton.me>
2025-07-05 11:57:13 +0200

    Backend: fixed auth configuration profile

--- src/main/java/dev/mednikov/accounting/shared/config/AuthenticationConfig.java
+++ src/main/java/dev/mednikov/accounting/shared/config/AuthenticationConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
