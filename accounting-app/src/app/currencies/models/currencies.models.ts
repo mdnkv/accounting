35b4e121306295761fa53b3d91494f3acc6ff3a7
Iurii Mednikov <yurimednikov@proton.me>
2025-08-05 09:21:32 +0200

    Frontend: removed old frontend

--- accounting-app/src/app/currencies/models/currencies.models.ts
+++
@@ -1,7 +0,0 @@
-export interface Currency {
-  id?: string
-  organizationId?: string
-  name: string
-  code: string
-  primary: boolean
-}
--- accounting-app/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  serverUrl: '/api',
-  keycloak: {
-    server: 'http://localhost:8080',
-    clientId: 'accounting-angular',
-    realm: 'accounting'
-  }
-};
