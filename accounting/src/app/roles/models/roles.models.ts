51bcde01c950e5cebfafaec9871654a9fc49990d
Iurii Mednikov <yurimednikov@proton.me>
2025-09-09 12:28:43 +0200

    Frontend: frontend guards for organizations

--- accounting/src/app/roles/models/roles.models.ts
+++ accounting/src/app/roles/models/roles.models.ts
@@ -1,5 +1,12 @@
+export interface Authority {
+  id?: string
+  organizationId?: string
+  name: string
+}
+
 export interface Role {
   id?: string
   organizationId?: string
   name: string
+  authorities: Authority[]
 }
